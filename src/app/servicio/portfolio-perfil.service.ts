d490a1e0a30621f94c85281f77773a30761c59f7
salvatierra1 <victordanielsalvatierra6@gmail.com>
2023-01-06 12:46:29 -0300

    add url

--- src/app/servicio/portfolio-perfil.service.ts
+++ src/app/servicio/portfolio-perfil.service.ts
@@ -10,7 +10,7 @@ export class PortfolioPerfilService {
  
   estaAuth:boolean;
   
-  url:string = "https://fathomless-cliffs-73656.herokuapp.com/profile/1";
+  url:string = "web-production-ae15.up.railway.app/profile/1";
  
    
   constructor(private http:HttpClient, private authsService:AuthService) { }
